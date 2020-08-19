dd0c1fcd85931c4e6c4c22d1dcb9a918bfd421a9
Valeriy <valeriykovalenko18@gmail.com>
2020-08-19 21:15:51 +0300

    adapt for mobile version

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".scroll-about, header__button").click(function () {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+    });
+});
