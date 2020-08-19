9cd3507435b3065ec74b7f270ac048b66ec6b6da
Valeriy <valeriykovalenko18@gmail.com>
2020-08-19 21:27:27 +0300

    fix animaition

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".scroll-about, header__button").click(function () {
+    $(".scroll-about, .header__button").click(function () {
     var elementClick = $(this).attr("href")
     var destination = $(elementClick).offset().top;
     jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
