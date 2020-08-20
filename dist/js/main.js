6e4b9a871e051026dccc683233787b00dbbfe7a7
Valeriy <valeriykovalenko18@gmail.com>
2020-08-20 21:44:45 +0300

    add progress line

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,3 +6,29 @@ $(document).ready(function() {
     return false;
     });
 });
+
+var offset = 600;
+$(window).scroll(function(){
+  var scrolltop = $(this).scrollTop();
+  $('.skills').each(function(){
+    if(scrolltop >= $(this).offset().top - offset) {
+        var html = document.getElementById("html");
+        var css = document.getElementById("css");
+        var jquery = document.getElementById("jquery");
+        var photoshop = document.getElementById("photoshop");
+        html.style.width = "90%";
+        css.style.width = "80%";
+        jquery.style.width = "30%";
+        photoshop.style.width = "70%";
+    }
+  });
+});
+
+// function myFunction() {
+//     myVar = setTimeout(showPage, 1600);
+// }
+
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
