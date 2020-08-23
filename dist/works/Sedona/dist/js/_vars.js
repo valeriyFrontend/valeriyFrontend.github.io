99519bd6e84df4a25eafc0f31096e127535ae1be
Valeriy <valeriykovalenko18@gmail.com>
2020-08-23 19:48:58 +0300

    add RunSmart Sedona BarberShop

---
+++ dist/works/Sedona/dist/js/_vars.js
@@ -0,0 +1,8 @@
+export default {
+    $window: window,
+    $document: document,
+    $html: document.documentElement,
+    $body: document.body,
+    $bannerSlider: document.querySelector('.banner-slider'),
+    $marketing: document.querySelector('.marketing'),
+  }
---
+++ dist/works/Sedona/dist/js/main.js
@@ -0,0 +1,8 @@
+import './vendor/focus-visible.min.js';
+import './components/main-slider';
+import './components/marketing';
+import {resizeContent} from './functions/resize';
+import {scrollTo} from './functions/smooth-scroll';
+import {disableScroll, enableScroll} from './functions/stop-scroll';
+
+//disableScroll(fix) // fix -> class of element with position: fixed
