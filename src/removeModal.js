c71c3df27e8c8c724d0720706ab873e15bc81159
Valeriy Kovalenko <57315743+valeriyFrontend@users.noreply.github.com>
2024-10-04 09:54:01 +0300

    Update removeModal.js

--- src/removeModal.js
+++ src/removeModal.js
@@ -1,9 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
+const addObserverIfDesiredNodeAvailable = () => {
   const element = document.querySelector('.modal--empty-title');
-
   console.log(element)
-
-  setInterval(() => console.log(element) , 1000)
   
   const observer = new MutationObserver((mutations) => {
     console.log(element, 1)
@@ -22,4 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('beforeunload', () => {
     observer.disconnect();
   });
-});
+};
+
+addObserverIfDesiredNodeAvailable();
